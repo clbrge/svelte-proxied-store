5dbae48756cb1cbda03ee5862157e845ba09abe3
Christophe Le Bars <clb@toort.net>
2022-01-01 21:00:30 +0100

    typo

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,8 @@ export default [
   {
     input: "./src/store.js",
     output: [
-      { file: "dist/index.es.js", format: "es" },
-      { file: "dist/index.umd.js", format: "umd", name: "svelte-proxied-store" },
+      { file: "dist/store.es.js", format: "es" },
+      { file: "dist/store.umd.js", format: "umd", name: "svelte-proxied-store" },
     ]
   },
   // {
