d0974c0d694c02e99a6ac5e8fe955b396f4afdf4
Christophe Le Bars <clb@toort.net>
2022-01-09 08:22:27 +0100

    add cleanup

--- test/index.js
+++ test/index.js
@@ -1,141 +1,144 @@
-import * as assert from 'assert';
-import { proxied } from '../src/store';
+import * as assert from 'assert'
+import { proxied } from '../src/store'
 
 describe('store', () => {
   describe('proxied', () => {
-	it('creates a proxied store', () => {
-	  const count = proxied();
-	  const values = [];
+    it('creates a proxied store', () => {
+      const count = proxied()
+      const values = []
 
-	  const unsubscribe = count.subscribe(o => {
-		if (o.v !== undefined) values.push(o.v);
-	  });
+      const unsubscribe = count.subscribe(o => {
+        if (o.v !== undefined) values.push(o.v)
+      })
 
-	  count.assign({v: 0});
-	  count.emit();
+      count.assign({ v: 0 })
+      count.emit()
 
-	  count.assign({v: 1});
-	  count.emit();
+      count.assign({ v: 1 })
+      count.emit()
 
-	  unsubscribe();
+      unsubscribe()
 
-	  count.assign({v: 2});
+      count.assign({ v: 2 })
 
-	  assert.deepEqual(values, [0, 1]);
-	});
+      assert.deepEqual(values, [0, 1])
+    })
 
-	it('delete property a proxied store', () => {
-	  const count = proxied();
-	  const values = [];
+    it('delete property a proxied store', () => {
+      const count = proxied()
+      const values = []
 
-	  const unsubscribe = count.subscribe(o => {
-		if (o.v !== undefined) values.push(o.v);
-	  });
+      const unsubscribe = count.subscribe(o => {
+        if (o.v !== undefined) values.push(o.v)
+      })
 
-	  count.assign({v: 0});
-	  count.emit();
+      count.assign({ v: 0 })
+      count.emit()
 
-	  count.delete('v');
-	  count.emit();
+      count.delete('v')
+      count.emit()
 
-	  unsubscribe();
+      unsubscribe()
 
-	  assert.deepEqual(values, [0]);
-	});
+      assert.deepEqual(values, [0])
+    })
 
-	it('emit argument has been assigned', () => {
-	  const count = proxied();
-	  const values = [];
+    it('emit argument has been assigned', () => {
+      const count = proxied()
+      const values = []
 
-	  const unsubscribe = count.subscribe(o => {
-		if (o.v !== undefined) values.push(o.v);
-	  });
+      const unsubscribe = count.subscribe(o => {
+        if (o.v !== undefined) values.push(o.v)
+      })
 
-	  count.emit({v: 'x'});
+      count.emit({ v: 'x' })
 
-	  unsubscribe();
+      unsubscribe()
 
-	  assert.deepEqual(values, ['x']);
-	});
+      assert.deepEqual(values, ['x'])
+    })
 
-	it('destructure proxied store', () => {
-	  const { delete: deleteProperty, subscribe, assign, emit } = proxied();
-	  const values = [];
+    it('destructure proxied store', () => {
+      const { delete: deleteProperty, subscribe, assign, emit } = proxied()
+      const values = []
 
-	  const unsubscribe = subscribe(o => {
-		if (o.v !== undefined) values.push(o.v);
-	  })
+      const unsubscribe = subscribe(o => {
+        if (o.v !== undefined) values.push(o.v)
+      })
 
-	  assign({v: 0});
-	  emit();
+      assign({ v: 0 })
+      emit()
 
-	  deleteProperty('v');
-	  emit();
+      deleteProperty('v')
+      emit()
 
-	  unsubscribe()
+      unsubscribe()
 
-	  assert.deepEqual(values, [0]);
-	});
+      assert.deepEqual(values, [0])
+    })
 
-	it('creates an empty proxied store', () => {
-	  const store = proxied();
-	  const values = [];
+    it('creates an empty proxied store', () => {
+      const store = proxied()
+      const values = []
 
-	  const unsubscribe = store.subscribe(p => {
-		values.push(p);
-	  });
+      const unsubscribe = store.subscribe(p => {
+        values.push(p)
+      })
 
-	  unsubscribe();
+      unsubscribe()
 
-	  assert.deepEqual(values, [{}]);
-	});
+      assert.deepEqual(values, [{}])
+    })
 
-	it('basic handler that increment property by 1', () => {
-	  const values = [];
+    it('basic handler that increment property by 1', () => {
+      const values = []
 
-	  const store = proxied({
-        get: function(internal, property) {
+      const store = proxied({
+        get: function (internal, property) {
           internal[property]++
           return Reflect.get(internal, property)
         }
       })
 
-	  store.assign({v: 1});
+      store.assign({ v: 1 })
 
-	  assert.equal(store.get('v'), 1);
+      assert.equal(store.get('v'), 1)
 
-	  const unsubscribe1 = store.subscribe(p => { values.push(p.v) });
-	  const unsubscribe2 = store.subscribe(p => { values.push(p.v) });
+      const unsubscribe1 = store.subscribe(p => {
+        values.push(p.v)
+      })
+      const unsubscribe2 = store.subscribe(p => {
+        values.push(p.v)
+      })
 
-	  assert.deepEqual(values, [2, 3]);
+      assert.deepEqual(values, [2, 3])
 
-	  store.emit();
+      store.emit()
 
-	  assert.deepEqual(values, [2, 3, 4, 5]);
+      assert.deepEqual(values, [2, 3, 4, 5])
 
-	  unsubscribe1();
-	  unsubscribe2();
+      unsubscribe1()
+      unsubscribe2()
 
-	  store.emit();
+      store.emit()
 
-	  assert.deepEqual(values, [2, 3, 4, 5]);
-	});
+      assert.deepEqual(values, [2, 3, 4, 5])
+    })
 
-	it('only calls subscriber once initially, including on resubscriptions', () => {
-	  const store = proxied()
-	  store.assign({v: 1});
+    it('only calls subscriber once initially, including on resubscriptions', () => {
+      const store = proxied()
+      store.assign({ v: 1 })
 
-	  let count1 = 0;
-	  let count2 = 0;
+      let count1 = 0
+      let count2 = 0
 
-	  store.subscribe(() => count1 += 1)();
-	  assert.equal(count1, 1);
+      store.subscribe(() => (count1 += 1))()
+      assert.equal(count1, 1)
 
-	  const unsubscribe = store.subscribe(() => count2 += 1);
-	  assert.equal(count2, 1);
+      const unsubscribe = store.subscribe(() => (count2 += 1))
+      assert.equal(count2, 1)
 
-	  unsubscribe();
-	});
-  });
-
-});
+      unsubscribe()
+    })
+  })
+})
