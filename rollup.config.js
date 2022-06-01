0eca62be22de096abd2533d33ae9dcee4154466e
Christophe Le Bars <clb@toort.net>
2022-06-01 16:23:00 +0200

    rename esm export

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,8 @@ export default [
   {
     input: "./src/store.js",
     output: [
-      { file: "dist/store.es.js", format: "es" },
+      { file: "dist/store.mjs", format: "es" },
       { file: "dist/store.umd.js", format: "umd", name: "svelte-proxied-store" },
     ]
-  },
-  // {
-  //   input: "./src/store.d.ts",
-  //   output: [{ file: "dist/store.d.ts", format: "es" }],
-  //   plugins: [dts()],
-  // },
+  }
 ]
